4e3faa13c89d8e095f2ac0db6d25975a4824949c
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-07-27 22:38:26 -0400

    Checking in a fix to make the lookup and deletion O(1), which before was not.

--- LRUCache/LRUCache.cs
+++ LRUCache/LRUCache.cs
@@ -7,47 +7,44 @@ namespace LRUCache.Implementation
     public class LRUCache<K, V>
     {
         private readonly int _maxCapacity = 0;
-        private readonly Dictionary<K, Node<V>> _LRUCache;
-        private Node<V> head = null;
-        private Node<V> tail = null; 
+        private readonly Dictionary<K, Node<V, K>> _LRUCache;
+        private Node<V, K> _head = null;
+        private Node<V, K> _tail = null;
 
         public LRUCache(int argMaxCapacity)
         {
             _maxCapacity = argMaxCapacity;
-            _LRUCache = new Dictionary<K, Node<V>>();
+            _LRUCache = new Dictionary<K, Node<V, K>>();
         }
 
         public void Insert(K key, V value)
         {
             if (_LRUCache.ContainsKey(key))
             {
-                Node<V> foundItem = _LRUCache.FirstOrDefault(l => l.Key.Equals(key)).Value;
-                MakeMostRecentlyUsed(foundItem);
+                MakeMostRecentlyUsed(_LRUCache[key]);
             }
 
             if (_LRUCache.Count >= _maxCapacity) RemoveLeastRecentlyUsed();
 
-            Node<V> insertedNode = new Node<V>(value);
+            Node<V, K> insertedNode = new Node<V, K>(value, key);
 
-            if (head == null)
+            if (_head == null)
             {
-                head = insertedNode;
-                tail = head;
+                _head = insertedNode;
+                _tail = _head;
             }
             else MakeMostRecentlyUsed(insertedNode);
 
             _LRUCache.Add(key, insertedNode);
         }
 
-        public Node<V> GetItem(K key)
+        public Node<V, K> GetItem(K key)
         {
             if (!_LRUCache.ContainsKey(key)) return null;
-            
-            Node<V> foundItem = _LRUCache.FirstOrDefault(l => l.Key.Equals(key)).Value;
 
-            MakeMostRecentlyUsed(foundItem);
+            MakeMostRecentlyUsed(_LRUCache[key]);
 
-            return foundItem;
+            return _LRUCache[key];
         }
 
         public int Size()
@@ -57,7 +54,7 @@ namespace LRUCache.Implementation
 
         public string CacheFeed()
         {
-            var headReference = head; 
+            var headReference = _head; 
             
             List<string> items = new List<string>();
 
@@ -72,41 +69,38 @@ namespace LRUCache.Implementation
 
         private void RemoveLeastRecentlyUsed()
         {
-            var key = _LRUCache.FirstOrDefault(l => l.Value.Equals(tail)).Key;
-
-            _LRUCache.Remove(key);
-
-            tail.Previous.Next = null;
-            tail = tail.Previous;
+            _LRUCache.Remove(_tail.Key);
+            _tail.Previous.Next = null;
+            _tail = _tail.Previous;
         }
 
-        private void MakeMostRecentlyUsed(Node<V> foundItem)
+        private void MakeMostRecentlyUsed(Node<V, K> foundItem)
         {
             // Newly inserted item bring to the top
             if (foundItem.Next == null && foundItem.Previous == null)
             {
-                foundItem.Next = head;
-                head.Previous = foundItem;
-                if (head.Next == null) tail = head;
-                head = foundItem;
+                foundItem.Next = _head;
+                _head.Previous = foundItem;
+                if (_head.Next == null) _tail = _head;
+                _head = foundItem;
             }
             // If it is the tail than bring it to the top
             else if (foundItem.Next == null && foundItem.Previous != null)
             {
                 foundItem.Previous.Next = null;
-                tail = foundItem.Previous;
-                foundItem.Next = head;
-                head.Previous = foundItem;
-                head = foundItem;
+                _tail = foundItem.Previous;
+                foundItem.Next = _head;
+                _head.Previous = foundItem;
+                _head = foundItem;
             }
             // If it is an element in between than bring it to the top
             else if (foundItem.Next != null && foundItem.Previous != null)
             {
                 foundItem.Previous.Next = foundItem.Next;
                 foundItem.Next.Previous = foundItem.Previous;
-                foundItem.Next = head;
-                head.Previous = foundItem;
-                head = foundItem;
+                foundItem.Next = _head;
+                _head.Previous = foundItem;
+                _head = foundItem;
             }
             // Last case would be to check if it is a head but if it is than there is no need to bring it to the top
         }
--- LRUCache/Node.cs
+++ LRUCache/Node.cs
@@ -1,14 +1,16 @@
 ﻿namespace LRUCache.Implementation
 {
-    public class Node<D>
+    public class Node<D, K>
     {
         public D Data { get; private set; }
-        public Node<D> Previous { get; set; }
-        public Node<D> Next { get; set; }
+        public K Key { get; private set; }
+        public Node<D, K> Previous { get; set; }
+        public Node<D, K> Next { get; set; }
 
-        public Node(D data)
+        public Node(D data, K key)
         {
             Data = data;
+            Key = key;
         }
     }
 }
